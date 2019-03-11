779fdde74c824cb54f14be9d912db6a9ff37798f
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2019-03-11 15:20:27 +1100

    set up repo (#1)
    * set up repo
    
    * remove semi colons

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as s from "./App.styled";
+
+class App extends React.Component {
+  public render() {
+    return <s.Container>Breakinotes</s.Container>;
+  }
+}
+
+export default App;
